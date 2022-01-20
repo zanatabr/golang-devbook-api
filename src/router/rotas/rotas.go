a977dcdf09bcea9cc6210edde0894fcc5dd87062
zanatabr <zanata@gmail.com>
2022-01-20 08:09:48 -0300

    71: Inserindo Rotas Dentro do Router + 72: Testando Rotas no Postman

--- src/router/rotas/rotas.go
+++ src/router/rotas/rotas.go
@@ -1,6 +1,10 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Rota: representa todas as rotas da API
 type Rota struct {
@@ -8,4 +12,18 @@ type Rota struct {
 	Metodo             string
 	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Configurar: Insere todas as Rotas no Router, e retorna o Router com todas as rotas configuradas
+//             a) Recebe um Router sem nenhuma Rota
+//             b) Configura todas as Rotas
+//             c) Devolve um Router preparado
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
 }
