7141f5eb284452ddd5654e71afe2a0c8e535357e
zanatabr <zanata@gmail.com>
2022-01-20 08:27:27 -0300

    73: Modelo de Usuário

---
+++ src/modelos/Usuario.go
@@ -0,0 +1,13 @@
+package modelos
+
+import "time"
+
+// Usuario: representa um Usuário que usa a rede social
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
