4ad9b5f82fa1d57ceb18f84937757d2aea86ab11
zanatabr <zanata@gmail.com>
2022-01-21 00:21:26 -0300

    74: Pacote Config

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,10 +9,14 @@ import (
 )
 
 func main() {
+
+	config.Carregar()
+	fmt.Println(config.StringConexaoBanco)
+
 	fmt.Println("Rodando API")
 
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
 }
