1d695a3f601ed45697993c3458358c7d0e4642ab
zanatabr <zanata@gmail.com>
2022-01-22 00:40:05 -0300

    80: Métodos para Validação de Usuário

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario: representa um Usuário que usa a rede social
 type Usuario struct {
@@ -10,4 +14,37 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+// Preparar: chama os métodos para validar e formatar o usuário recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar vazio")
+	}
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar vazio")
+	}
+	if usuario.Email == "" {
+		return errors.New("O e-mail é obrigatório e não pode estar vazio")
+	}
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode estar vazia")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace((usuario.Nome))
+	usuario.Nick = strings.TrimSpace((usuario.Nick))
+	usuario.Email = strings.TrimSpace((usuario.Email))
 }
