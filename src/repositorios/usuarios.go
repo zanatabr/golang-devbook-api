1737ffe5b4d78a01af15068147577c168464f2d5
zanatabr <zanata@gmail.com>
2022-01-31 14:21:53 -0300

    87: Login

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -139,3 +139,23 @@ func (repositorio Usuarios) Deletar(ID uint64) error {
 	return nil
 
 }
+
+// BuscarPorEmail: busca um usuário por e-mail e retorna o seu id e senha com hash
+func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error) {
+	linha, erro := repositorio.db.Query("SELECT id, senha FROM usuarios WHERE email = ?", email)
+	if erro != nil {
+		return modelos.Usuario{}, erro
+	}
+	defer linha.Close()
+
+	var usuario modelos.Usuario
+
+	if linha.Next() {
+		if erro = linha.Scan(&usuario.ID, &usuario.Senha); erro != nil {
+			return modelos.Usuario{}, erro
+		}
+	}
+
+	return usuario, nil
+
+}
--- src/router/rotas/rotas.go
+++ src/router/rotas/rotas.go
@@ -20,6 +20,7 @@ type Rota struct {
 //             c) Devolve um Router preparado
 func Configurar(r *mux.Router) *mux.Router {
 	rotas := rotasUsuarios
+	rotas = append(rotas, rotaLogin) // faz o append da rota de login
 
 	for _, rota := range rotas {
 		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
