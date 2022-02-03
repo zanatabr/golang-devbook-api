bdacda19398b0ec288ccdfb76be5e85c77a292b0
zanatabr <zanata@gmail.com>
2022-02-03 18:40:56 -0300

    100: Modelo de Publicação

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
-
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
 
@@ -26,4 +26,14 @@ CREATE TABLE seguidores(
     REFERENCES usuarios(id)
     ON DELETE CASCADE,
     PRIMARY KEY(usuario_id, seguidor_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    autor_id int not null,
+    FOREIGN KEY (autor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp()
 ) ENGINE=INNODB;
