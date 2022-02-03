79992be96bff01e7d692ed11a6784dc418c0b56d
zanatabr <zanata@gmail.com>
2022-02-03 14:55:42 -0300

    96: Parar de Seguir Usuário

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -237,3 +237,41 @@ func SeguirUsuario(w http.ResponseWriter, r *http.Request) {
 	respostas.JSON(w, http.StatusNoContent, nil)
 
 }
+
+// PararDeSeguirUsuario: permite que um usuário pare de seguir outro
+func PararDeSeguirUsuario(w http.ResponseWriter, r *http.Request) {
+
+	seguidorID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	parametros := mux.Vars(r)
+
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if usuarioID == seguidorID {
+		respostas.Erro(w, http.StatusForbidden, errors.New("não é possível deixar de seguir você mesmo"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	if erro = repositorio.PararDeSeguir(usuarioID, seguidorID); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
+
+}
--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -160,10 +160,28 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 
 }
 
-// Seguir: insere um usuario no BD
+// Seguir: permite que um usuário siga outro
 func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
 	statement, erro := repositorio.db.Prepare(
 		"insert ignore into seguidores (usuario_id, seguidor_id) values (?, ?)",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+
+}
+
+// PararDeSeguir: permite que um usuário deixe de seguir outro
+func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"DELETE FROM seguidores WHERE usuario_id = ? AND seguidor_id = ?",
 	)
 	if erro != nil {
 		return erro
