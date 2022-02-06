737598588c8f0ae11f8ec3645bba070256e47bd3
zanatabr <zanata@gmail.com>
2022-02-06 14:51:36 -0300

    108: Busca de Publicações de um Usuário

--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -223,3 +223,32 @@ func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
 	}
 	respostas.JSON(w, http.StatusNoContent, nil)
 }
+
+// BuscarPublicacoesPorUsuario: traz as publicações de um usuário
+func BuscarPublicacoesPorUsuario(w http.ResponseWriter, r *http.Request) {
+
+	parametros := mux.Vars(r)
+
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.BuscarPorUsuario(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacoes)
+
+}
--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -149,3 +149,42 @@ func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
 	return nil
 
 }
+
+// BuscarPorUsuario: traz as publicações de um usuário
+func (repositorio Publicacoes) BuscarPorUsuario(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+		SELECT DISTINCT p.*, u.nick
+		  FROM publicacoes p 
+		 INNER JOIN usuarios u ON u.id = p.autor_id
+		 WHERE p.autor_id = ?
+		 ORDER BY 1`,
+		usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+
+		var publicacao modelos.Publicacao
+
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
